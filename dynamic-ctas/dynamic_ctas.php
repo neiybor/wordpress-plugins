7ebd60a3226d84f0e99799515f7bd375d9f4474e
Dallin Anderson <dallin.anderson@neighbor.com>
2024-03-07 14:20:20 -0700

    Add ab-testing

--- dynamic-ctas/dynamic_ctas.php
+++ dynamic-ctas/dynamic_ctas.php
@@ -116,6 +116,21 @@ function nbrdcta_add_handlers()
 add_action('after_setup_theme', 'nbrdcta_add_handlers', 150);
 
 /**
+ * Set the anonymous id for the user
+ */
+$nbrdcta_anonymous_id = $_COOKIE['nbrdcta_anonymous_id'] ?? "";
+add_action('init', 'nbrdcta_set_anonymous_id');
+function nbrdcta_set_anonymous_id()
+{
+  global $nbrdcta_anonymous_id;
+  if (empty($nbrdcta_anonymous_id)) {
+    $nbrdcta_anonymous_id = bin2hex(random_bytes(16));
+    // expires in 30 days
+    setcookie('nbrdcta_anonymous_id', $nbrdcta_anonymous_id, time() + (86400 * 30), "/");
+  }
+}
+
+/**
  * Handle adding 1st CTA to post body
  */
 function nbrdcta_handle_1_cta_body($content)
@@ -270,5 +285,167 @@ function nbrdcta_get_custom_html($key)
   if (!$insert_html) {
     return "";
   }
-  return $insert_html;
+  return nbrdcta_get_ab_test_content($insert_html);
+}
+
+/**
+ * Handle determining if there's an ab test and return content for arm
+ */
+function nbrdcta_get_ab_test_content($html)
+{
+  // check if the first div of provided $html has a class beginning with "AB_editorial"
+  $dom = new DOMDocument;
+  // The @ sign supresses warnings we are seeing. Not a permanent fix
+  $succeeded = @$dom->loadHTML($html);
+  if (!$succeeded) {
+    return $html;
+  }
+  $divs = $dom->getElementsByTagName('div');
+  if ($divs->length == 0) {
+    return $html;
+  }
+  $first_div = $divs->item(0);
+  $class = $first_div->getAttribute('class');
+  if (strpos($class, 'AB_editorial_') === 0) {
+    $test_name = substr($class, strlen('AB_editorial_'));
+    $arm = nbrdcta_assign_ab_test_arm($test_name);
+    if ($arm) {
+      $html = nbrdcta_get_ab_test_arm_content($html, $arm);
+    }
+  }
+  return $html;
+}
+
+/**
+ * Assign an arm to the user
+ * Returns "control" or "variant"
+ */
+function nbrdcta_assign_ab_test_arm($test_name)
+{
+  global $nbrdcta_anonymous_id;
+  if (empty($nbrdcta_anonymous_id)) {
+    return "control";
+  }
+
+
+  // $anonymous_id = nbrdcta_get_anonymous_id();
+  $arm = nbrdcta_get_test_arm($test_name, $nbrdcta_anonymous_id);
+  nbrdcta_send_analytics_event($test_name, $arm);
+  
+  return $arm;
+}
+add_action('wp_footer', function() use ($nbrdcta_anonymous_id) {
+  global $nbrdcta_anonymous_id;
+  nbrdcta_console_log($nbrdcta_anonymous_id);
+}, 100);
+function nbrdcta_console_log($message)
+{
+  ?>
+  <script>
+    console.log("<?php echo "message: $message"; ?>");
+  </script>
+  <?php
+}
+
+/**
+ * Get/Set anonymous id from cookies
+ */
+// function nbrdcta_get_anonymous_id()
+// {
+//   $anonymous_id = $_COOKIE['nbrdcta_anonymous_id'] ?? "";
+//   if (empty($anonymous_id)) {
+//     $anonymous_id = bin2hex(random_bytes(16));
+//     // expires in 30 days
+//     setcookie('nbrdcta_anonymous_id', $anonymous_id, time() + (86400 * 30), "/");
+//   }
+//   return $anonymous_id;
+// }
+
+/**
+ * Send an event to analytics
+ */
+function nbrdcta_send_analytics_event($test_name, $arm)
+{
+  $full_test_name = "AB_editorial_" . $test_name;
+  // add script tag to send event to analytics
+  add_action('wp_footer', function() use ($full_test_name, $arm) {
+    nbrdcta_prepare_analytics_script($full_test_name, $arm);
+  }, 100);
+
+}
+
+/**
+ * Add script tag to send event to analytics
+ */
+function nbrdcta_prepare_analytics_script($full_test_name, $arm)
+{
+  ?>
+    <script>
+      // Your JavaScript code here
+      console.log('Hello, world!');
+      var fullTestName = "<?php echo "$full_test_name"; ?>";
+      var arm = "<?php echo "$arm"; ?>";
+      console.log('fullTestName: ', fullTestName);
+      console.log('arm: ', arm);
+      rudderanalytics.identify({ [fullTestName]: arm });
+    </script>
+  <?php
+}
+
+/**
+ * Consitently returns the same test arm based on a hash composed from test_name and anon_id
+ */
+define("MAX_UNSIGNED_32BIT", 4294967295);
+function nbrdcta_get_test_arm($test_name, $anon_id)
+{
+  $hashed_index = hexdec(hash('murmur3a', $test_name . "_" . $anon_id));
+  $arms = [['control', 0.5], ['variant', 0.5]];
+  $weight_sum = array_reduce($arms, function($sum, $arm) {
+    return $sum + $arm[1];
+  }, 0);
+  $normalized_weights = [];
+  array_reduce($arms, function($prev_range_end, $arm) use (&$normalized_weights, $weight_sum) {
+    $amount_of_range = round(($arm[1] / $weight_sum) * MAX_UNSIGNED_32BIT);
+    $end_of_range = $prev_range_end + $amount_of_range;
+    if ($amount_of_range > 0) {
+      $normalized_weights[] = [$arm[0], $end_of_range];
+    }
+    return $end_of_range;
+  }, 0);
+
+  $matching_arms = array_filter($normalized_weights, function($weight) use ($hashed_index) {
+    $end_of_range = $weight[1];
+    return $hashed_index <= $end_of_range;
+  });
+
+  $assigned_arm = reset($matching_arms) ?: end($normalized_weights);
+  return $assigned_arm[0];
+}
+
+/**
+ * Retrieve just the div with the class matching the arm ("control" or "variant")
+ * Ex: $html = "<div class="AB_editorial_test_name"><div class="control">Control</div><div class="variant">Variant</div></div>" => <div class="variant">Variant</div>
+ */
+function nbrdcta_get_ab_test_arm_content($html, $arm)
+{
+  $dom = new DOMDocument;
+  // The @ sign supresses warnings we are seeing. Not a permanent fix
+  $succeeded = @$dom->loadHTML($html);
+  if (!$succeeded) {
+    return $html;
+  }
+  $divs = $dom->getElementsByTagName('div');
+  if ($divs->length == 0) {
+    return $html;
+  }
+  $first_div = $divs->item(0);
+  $children = $first_div->childNodes;
+  foreach ($children as $child) {
+    if ($child->nodeType == XML_ELEMENT_NODE) {
+      if ($child->getAttribute('class') == $arm) {
+        return $dom->saveHTML($child);
+      }
+    }
+  }
+  return $html;
 }
